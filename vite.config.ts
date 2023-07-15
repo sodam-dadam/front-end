ac88cd5cbf665c0d017ea995f1b8d47ff182ce0c
JungHo LEE <66310772+wjdgh4058@users.noreply.github.com>
2023-07-15 19:56:00 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import rollupNodePolyFill from "rollup-plugin-polyfill-node";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            timers: "rollup-plugin-node-polyfills/polyfills/timers",
+        },
+    },
+    build: {
+        plugins: [
+            rollupNodePolyFill(),
+           react()
+        ],
+    },
+});
