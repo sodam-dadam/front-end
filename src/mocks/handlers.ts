f722bf434242b420e51cb89de5509a210c6b69d2
leejungho <ljhmoa@gmail.com>
2023-07-15 20:44:22 +0900

    refactor: 배포 url 로 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('http://localhost:3000/start', (req, res, ctx) => {
+  rest.get('http://sodamdadam.s3-website.us-east-2.amazonaws.com/start', (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -10,7 +10,7 @@ export const handlers = [
     );
   }),
 
-  rest.get('http://localhost:3000/', (req, res, ctx) => {
+  rest.get('http://sodamdadam.s3-website.us-east-2.amazonaws.com/', (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
