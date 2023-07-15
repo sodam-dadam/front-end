d98e6e9b17bf4c15c28bb4d241bec478dc89a7c1
leejungho <ljhmoa@gmail.com>
2023-07-15 20:07:08 +0900

    fix: vite.config.ts 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import rollupNodePolyFill from "rollup-plugin-polyfill-node";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            timers: "rollup-plugin-node-polyfills/polyfills/timers",
-        },
-    },
-    build: {
-        plugins: [
-            rollupNodePolyFill(),
-           react()
-        ],
-    },
+  plugins: [react()],
 });
